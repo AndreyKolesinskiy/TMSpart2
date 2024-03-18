105b6c0f4e82a549d51344df79078ed5d364c09c
Andrei Kaliasinski <andykolesinski@gmail.com>
2024-03-18 07:59:37 +0300

    Add project files.

---
+++ TMSpart2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMSpart2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Exercize1();
+        }
+
+        static void Exercize1()
+        {
+            string initialString = "11, -21, 64, 59, 11, 0, -7, 53, -31, 1";
+            string initialStringWithoutSpaces = initialString.Replace(" ", "");
+
+            Console.WriteLine(initialStringWithoutSpaces);
+            List<int> listOfNumbers = initialStringWithoutSpaces.Split(',').Select(int.Parse).ToList();
+          
+            listOfNumbers.Sort();
+            foreach (int number in listOfNumbers)
+            {
+                if (number >= 10) { Console.WriteLine(number); }
+
+            }
+        }
+    }
+}
---
+++ TMSpart2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TMSpart2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TMSpart2")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("14fa4a19-295a-45a0-8c38-ed836c5a1699")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
